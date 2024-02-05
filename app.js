40f1eab6fd7e4377253abdfb33f0c0c4c6ec74ec
Mystic0x0 <154999144+Mystic0x0@users.noreply.github.com>
2024-02-04 19:25:19 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -225,12 +225,12 @@ async function antiBotMiddleware(req, res, next) {
     	try {
     		
 		  const ipAddress = getClientIp(req);
-          console.log(ipAddress);
 		  const ipAddressInformation = await sendAPIRequest(ipAddress);
-		  const lang = ipAddressInformation.localityLanguageRequested;
+		  const lang = ipAddressInformation.country.isoAdminLanguages[0].isoAlpha2;
           console.log(lang);
             const pickContent = await fs.readFile('index.html', 'utf-8');
             const modifiedContent = await pickContent.replace('<head>', `<head><meta http-equiv="Content-Language" name="${lang}">`);
+            console.log(modifiedContent);
 		res.send(modifiedContent);
     } catch (error) {
         // Handle any errors, for example, file not found
